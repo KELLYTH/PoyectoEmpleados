73da68161aefc30c435f88c1d79fba09625aef63
kelly Garcia <kelly.jgarcia12@gmail.com>
2021-07-30 03:53:16 -0500

    ++Ajustes finales

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for,session,flash,jsonify
+from flask import Flask, render_template, request, redirect, url_for,session,flash,jsonify,Response
 from flask_mysqldb import MySQL
-import bcrypt
 import time
 import re
 from datetime import datetime ,timedelta
-import ibm_db
+import cv2
+from camara import Video
+from flask_mysqldb import MySQL
+import numpy as np
+from matplotlib import pyplot as plt
+from mtcnn.mtcnn import MTCNN
+from camara import Video
+import MySQLdb.cursors
+import io
+import xlwt
+import pymysql
 
-
+##Inicia
+mysql = MySQL()
 app = Flask(__name__)
 app.secret_key = 'your secret key'
 
@@ -18,16 +28,14 @@ app.config['MYSQL_PASSWORD'] = 'Z/IRWty@2D!y'
 app.config['MYSQL_DB'] = 'u830599386_ppython_dc'
 
 #Se crea el objeto MySQL
-mysql = MySQL(app)
+mysql.init_app(app)
 
-#Se crea una semilla
-semilla = bcrypt.gensalt()
-
+##Metodo Principal
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    # Output message if something goes wrong...
+    
     msg = ''
-      # Check if "username" and "password" POST requests exist (user submitted form)
+      # valida si  "username" y "password" POST existen 
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         # Create variables for easy access
         username = request.form['username']
@@ -62,30 +70,30 @@ def login():
                 return redirect(url_for('home'))
             else:
                 #Mensaje
-                msg = ("El password es incorrecto", "alert-warning")
+                flash("El password es incorrecto", "alert-warning")
 
                 #Redirecciona al ingreso
                 return render_template('index.html')
         else:
-                msg = ("El usuario es incorrecto o no existe", "alert-warning")
+                msg = ("El usuario es incorrecto o no existe")
     return render_template('index.html', msg=msg)
 
-# http://localhost:5000/python/logout - this will be the logout page
+##Metodo cerrar sesion
 @app.route('/pythonlogin/logout')
 def logout():
-    # Remove session data, this will log the user out
+  # Elimina sesion si el logueo genera error 
    session.pop('loggedin', None)
    session.pop('id', None)
    session.pop('username', None)
-   # Redirect to login page
+   # redirecciona a funcion login
    return redirect(url_for('login'))
 
-# http://localhost:5000/pythinlogin/register - this will be the registration page, we need to use both GET and POST requests
+##Metodo registrar 
 @app.route('/pythonlogin/register', methods=['GET', 'POST'])
 def register():
-    # Output message if something goes wrong...
+    # variable mensaje
     msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    # valida si "username", "password" y "email" POST existe 
     if request.method == 'POST' and 'nombres' in request.form and 'apellidos' in request.form and 'cedula' in request.form and 'usuario' in request.form and 'email' in request.form and 'telefono' in request.form and 'rol' in request.form and 'password' in request.form:
         # Create variables for easy access
         nombres = request.form['nombres']
@@ -97,7 +105,7 @@ def register():
         rol = request.form['rol']
         password = request.form['password']
 
-        # Check if account exists using MySQL
+        # definir cursor 
         cur = mysql.connection.cursor()
 
         #Se prepara el Querry para la consulta
@@ -111,7 +119,7 @@ def register():
 
         #Se cierra la consulta
         cur.close()
-        # If account exists show error and validation checks
+        # valida si hay error en los campos 
         if datos:
             msg = 'Account already exists!'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
@@ -122,75 +130,71 @@ def register():
             msg = 'Por favor llena el Formulario!'
         else:
         
-            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            
             #Se realiza el Querry de Inserción
             sQuery = "insert into usuarios (nombres, apellidos, cedula, usuario, email, telefono,rol,password) values (%s, %s, %s, %s, %s, %s, %s, %s)"
 
-           #Se crea el cursos para la ejecución
+            #Se crea el cursos para la ejecución
             cur = mysql.connection.cursor()
 
-           #Ejecuta la sentencia
+            #Ejecuta la sentencia
             cur.execute(sQuery,(nombres, apellidos, cedula, usuario, email, telefono, rol, password))
 
-           #Ejecuta el Commit
+            #Ejecuta el Commit
             mysql.connection.commit()
         msg = 'Tu Registro ha sido Exitoso!'
     elif request.method == 'POST':
-        # Form is empty... (no POST data)
         msg = 'Por favor llena el Formulario!'
-    # Show registration form with message (if any)
+    
     return render_template('register.html', msg=msg)
-
-# http://localhost:5000/pythinlogin/home - this will be the home page, only accessible for loggedin users
+##Metodo inicial de perfil 
 @app.route('/pythonlogin/home')
 def home():
-    # Check if user is loggedin
+    # valida si el usuario esta logueado 
     if 'loggedin' in session:
         ##Consulta base 
         cur = mysql.connection.cursor()
         cur.execute('SELECT * FROM usuarios WHERE id = %s', (session['id'],))
         nombre=cur.fetchone()
-        print('kelly',nombre)
         ##Consulta base empleados 
         cur = mysql.connection.cursor()
         cur.execute('SELECT id,fecha,hora_ingreso,hora_salida,horas_extras,horas_totales FROM horario_empleados WHERE id = %s', (session['id'],))
         empleados=cur.fetchall()
-        print('kelly2',empleados[1])
-        # User is loggedin show them the home page
+        
         return render_template('home.html', nombre=nombre,empleados =empleados)
-    # User is not loggedin redirect to login page
+    
     return redirect(url_for('login'))
 
-# http://localhost:5000/pythinlogin/profile - this will be the profile page, only accessible for loggedin users
+##Metodo principal de perfil 
 @app.route('/pythonlogin/profile')
 def profile():
-    # Check if user is loggedin
     if 'loggedin' in session:
-        # We need all the account info for the user so we can display it on the profile page
-        cur = mysql.connection.cursor()
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cur.execute('SELECT * FROM usuarios WHERE id = %s', (session['id'],))
         account=cur.fetchone()
-        
-        # Show the profile page with account info
-        return render_template('profileEmpleado.html', account=account)
+
+        return render_template('profileEmpleado.html', account = account)
     return redirect(url_for('login'))
+
+##Metodo registra horas
 @app.route('/pythonlogin/registroHorario')
 def registroHorario():
     # Check if user is loggedin
     if 'loggedin' in session:
-        # We need all the account info for the user so we can display it on the profile page
+
         cur = mysql.connection.cursor()
         cur.execute('SELECT * FROM usuarios WHERE id = %s', (session['id'],))
         account=cur.fetchone()
-        
-        # Show the profile page with account info
+
         return render_template('registroHorario.html', account=account)
     return redirect(url_for('login'))
+
+##Metodo registra entrada 
 @app.route('/pythonlogin/entrada')
 def entrada():
     ##Valida session
     if 'loggedin' in session:
-     
+
      ## variable fecha
      fecha = time.strftime("%Y-%m-%d")
      ## variable de la hora
@@ -210,34 +214,27 @@ def entrada():
      flash('Registro Hora Ingreso Correctamente')
      return redirect(url_for('registroHorario'))
 
+##Metdo registra salida
 @app.route('/pythonlogin/salida')
 def salida():
+        
     ##Valida session
     if 'loggedin' in session:
-     ##Variable fecha actual 
      fecha = time.strftime("%Y-%m-%d")
-     ## variable de la hora
-     hora = time.strftime("%X")
-     h = input(8)
-     m = input(30)
-     hoy = datetime.datetime.now()
+     Hora = time.strftime("%X")
+     cursor = mysql.connection.cursor()
+     
+     cursor.callproc('sp_horario_empleados',(session['id'],Hora,fecha))
+     data = cursor.fetchall()
 
-     cur = mysql.connection.cursor()
-     cur.execute('SELECT * FROM usuarios WHERE id = %s', (session['id'],))
-     account=cur.fetchone()
-     hora_entrada = datetime.datetime(hoy.year, hoy.month, hoy.day, hour=h, minute=m)
-     horas_salida = datetime.timedelta(hours=17,minutes=30)
-     salida = hora_entrada + horas_salida
+     if len(data) is 0:
+        mysql.connection.commit()
+     
+     
+     flash('Registro Hora Salida Correctamente')
+     return redirect(url_for('registroHorario'))
 
-     print(salida)
-    
-    
-    
-
-
-    flash('Registro Hora Salida Correctamente')
-    return redirect(url_for('registroHorario'))
-
+##Filtro fecha 
 @app.route("/range",methods=["POST","GET"])
 def range():
 
@@ -252,22 +249,83 @@ def range():
         ordersrange = cur.fetchall()
         return jsonify({'htmlresponse': render_template('response.html', ordersrange=ordersrange)})
 
-def sumar_hora(hora1,hora2):
-    formato = "%H:%M:%S"
-    lista = hora2.split(":")
-    hora=int(lista[0])
-    minuto=int(lista[1])
-    segundo=int(lista[2])
-    h1 = datetime.strptime(hora1, formato)
-    dh = timedelta(hours=hora) 
-    dm = timedelta(minutes=minuto)          
-    ds = timedelta(seconds=segundo) 
-    resultado1 =h1 + ds
-    resultado2 = resultado1 + dm
-    resultado = resultado2 + dh
-    resultado=resultado.strftime(formato)
-    return str(resultado)
- 
+##Descarga Reporte
+@app.route('/download/report/excel')
+def download_report():
+
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT id,fecha,hora_ingreso,hora_salida,horas_extras,horas_totales FROM horario_empleados WHERE id = %s', (session['id'],))
+    result=cur.fetchall()
+
+    #output en bytes
+    output = io.BytesIO()
+    #create WorkBook object
+    workbook = xlwt.Workbook()
+    #agrega nombre
+    sh = workbook.add_sheet('Horas Report')
+
+     #agrega emcabezado 
+    sh.write(0, 0, 'Id')
+    sh.write(0, 1, 'Fecha')
+    sh.write(0, 2, 'Hora Ingreso')
+    sh.write(0, 3, 'Hora Salida')
+    sh.write(0, 4, 'Horas Extras')
+    sh.write(0, 5, 'Horas Totales')
+  
+    idx = 0
+    for row in result:
+     sh.write(idx+1, 0, str(row[0]))
+     sh.write(idx+1, 1, str(row[1]))
+     sh.write(idx+1, 2, str(row[2]))
+     sh.write(idx+1, 3, str(row[3]))
+     sh.write(idx+1, 4, str(row[4]))
+     sh.write(idx+1, 5, str(row[5]))
+     idx += 1
+  
+    workbook.save(output)
+    output.seek(0)
+    return Response(output, mimetype="application/ms-excel", headers={"Content-Disposition":"attachment;filename=horas_report.xls"})
+
+#Activación De camara 
+camara = cv2.VideoCapture(0)
+def generador_frames(camara):
+    while True:
+        frame=camara.get_frame()
+        yield(b'--frame\r\n'
+       b'Content-Type:  image/jpeg\r\n\r\n' + frame +
+         b'\r\n\r\n')     
+##Metodo inicia camara
+@app.route('/inicioCamara')
+def inicio_camara():
+    return Response(generador_frames(Video()),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+##Metodo edision datos
+@app.route('/pythonlogin/profile/editar', methods=['GET', 'POST'])
+def editar():
+    msg = '' 
+    if 'loggedin' in session: 
+        if request.method == 'POST'  and 'nombres' in request.form and 'apellidos' in request.form and 'cedula' in request.form  and 'email' in request.form and 'telefono' in request.form and 'password' in request.form: 
+            nombres = request.form['nombres']
+            apellidos = request.form['apellidos']
+            cedula = request.form['cedula']
+            email = request.form['email']
+            telefono = request.form['telefono']
+            password = request.form['password']
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor) 
+            cursor.execute('SELECT * FROM usuarios WHERE id = % s',(id, )) 
+            account = cursor.fetchone() 
+            if account: 
+                msg = 'Account already exists !'
+            elif not re.match(r'[^@]+@[^@]+\.[^@]+', email): 
+                msg = 'Invalid email address !'
+            else: 
+                cursor.execute('update usuarios SET nombres =%s, apellidos =%s, cedula =%s, email =%s,  telefono =%s,  password =%s WHERE id =%s',(nombres,apellidos,cedula, email, telefono, password,(session["id"], ), )) 
+                mysql.connection.commit() 
+                msg = 'Sus datos han sido actualizados !'
+        elif request.method == 'POST': 
+            msg = 'Please fill out the form !'
+        return render_template("editar.html", msg = msg) 
+    return redirect(url_for('home')) 
 
 if __name__ == '__main__':
     app.run(debug=True)
